455c4dd7b86adfb8352f08ed889ac793fa21fcbd
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-02 11:11:30 -0300

    criando estrutura do projeto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
+const { response } = require('express')
 const express = require('express')
 
 const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: "Hello world Ignite!" })
+})
 
 app.listen(3033, () => console.log("server running in http://localhost:3033"))
